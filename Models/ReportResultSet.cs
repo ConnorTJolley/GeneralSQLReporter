1f24fba116db91453033a9544f55a50b69c8a355
Connor Jolley <connorjolley18@gmail.com>
2023-04-19 18:45:58 +0100

    Done the Following:
    - [#5] - Added the ability to Export a `ReportResultSet` to HTML.
    - [#17] - Added the ability to Specify a Template for the HTML Export

--- Models/ReportResultSet.cs
+++ Models/ReportResultSet.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Data.SqlClient;
+    using GeneralSQLReporter.Enums;
     using GeneralSQLReporter.Helpers;
     using Newtonsoft.Json;
 
