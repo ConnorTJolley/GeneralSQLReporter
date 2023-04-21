0d50c4af476e75752f4734d00e0dbc405bfae0e4
Connor Jolley <connorjolley18@gmail.com>
2023-04-21 18:57:38 +0100

    Done the following:
    - [#21] Added ability to specify a FileName for the HTML Export
    - [#2] Added ability to create & save a CSV export of the Report
    - Done some general cleanup of code, added SR classes like `HtmlExporter` and `CsvExporter` to contain code for their purpose.

---
+++ Helpers/Exporters/CsvExporter.cs
@@ -0,0 +1,68 @@
+﻿namespace GeneralSQLReporter.Helpers.Exporters
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using GeneralSQLReporter.Models;
+
+    /// <summary>
+    /// Interaction Logic for the Helper Class <see cref="CsvExporter"/>
+    /// </summary>
+    /// <remarks>
+    /// Handles the Generating of the <see cref="ReportResultSet"/> into a CSV Document
+    /// </remarks>
+    internal static class CsvExporter
+    {
+        /// <summary>
+        /// <inheritdoc cref="SqlReportExporter.ExportCsv(ReportResultSet, bool, bool, string, char)"/>
+        /// </summary>
+        internal static string ExportCsv(ReportResultSet report,
+            bool includeColumns,
+            bool overwrite, 
+            string fileName,
+            char delimeter)
+        {
+            SqlReportExporter.CheckExportFolder();
+
+            var fullPath = string.IsNullOrWhiteSpace(fileName?.Trim()) ?
+                Path.Combine(SqlReportExporter.OutputDirectory(), $"{Guid.NewGuid()}.csv") :
+                Path.Combine(SqlReportExporter.OutputDirectory(), fileName);
+
+            var builder = new StringBuilder();
+
+            if (includeColumns)
+            {
+                var colString = string.Empty;
+                report.Columns.ForEach(col =>
+                {
+                    colString += $"{col.Name}{delimeter}"; //// Remove last delimeter
+                });
+
+                colString = colString.Remove(colString.Length - 1, 1);
+                builder.Append(colString).Append(Environment.NewLine);
+            }
+
+            var rows = report.Rows;
+            rows.ForEach(r =>
+            {
+                var fullRow = string.Empty;
+
+                r.Values.ForEach(val =>
+                {
+                    fullRow += $"{val.Value}{delimeter}";
+                });
+
+                fullRow = fullRow.Remove(fullRow.Length - 1, 1); //// Remove last delimeter
+                builder.Append(fullRow).Append(Environment.NewLine);
+            });
+
+            if (File.Exists(fullPath) && overwrite)
+            {
+                File.Delete(fullPath);
+            }
+
+            File.WriteAllText(fullPath, builder.ToString());
+            return fullPath;
+        }
+    }
+}
---
+++ Helpers/Exporters/HtmlExporter.cs
@@ -0,0 +1,101 @@
+﻿namespace GeneralSQLReporter.Helpers.Exporters
+{
+    using System;
+    using System.IO;
+    using GeneralSQLReporter.Models;
+
+    /// <summary>
+    /// Interaction Logic for the Helper Class <see cref="HtmlExporter"/>
+    /// </summary>
+    /// <remarks>
+    /// Handles the Generating of the <see cref="ReportResultSet"/> into a HTML Document
+    /// </remarks>
+    internal static class HtmlExporter
+    {
+        /// <summary>
+        /// <inheritdoc cref="SqlReportExporter.ExportHtml(ReportResultSet, string, bool, string)"/>
+        /// </summary>
+        internal static string ExportHtmlBase(ReportResultSet report,
+            string templatePath = null,
+            bool overwrite = true,
+            string fileName = null)
+        {
+            SqlReportExporter.CheckExportFolder();
+
+            var generatedHtml = HtmlExporter.GenerateBaseHtml(report, templatePath);
+            var fullPath = string.IsNullOrWhiteSpace(fileName?.Trim()) ?
+                Path.Combine(SqlReportExporter.OutputDirectory(), $"{Guid.NewGuid()}.html") :
+                Path.Combine(SqlReportExporter.OutputDirectory(), fileName);
+
+            if (File.Exists(fullPath) && overwrite)
+            {
+                File.Delete(fullPath);
+            }
+
+            File.WriteAllText(fullPath, generatedHtml);
+            return fullPath;
+        }
+
+        /// <summary>
+        /// Handles Generating the HTML Report contents.
+        /// </summary>
+        /// <param name="report">The <see cref="ReportResultSet"/> to generate the HTML Report of</param>
+        /// <param name="templatePath">The path to the Template HTML to use if set</param>
+        /// <returns>The generate HTML report content to be saved</returns>
+        /// <exception cref="Exception">Throws an <see cref="Exception"/> if the BaseHtml template resource was null or empty.</exception>
+        internal static string GenerateBaseHtml(ReportResultSet report, string templatePath)
+        {
+            var usingTemplate = !string.IsNullOrWhiteSpace(templatePath?.Trim());
+
+            var baseHtml = !usingTemplate ?
+                Resource.html_template :
+                File.ReadAllText(templatePath?.Trim());
+
+            if (string.IsNullOrWhiteSpace(baseHtml))
+            {
+                throw new Exception($"Resource was missing, please raise an issue and inform me.");
+            }
+
+            baseHtml = baseHtml.Replace("[COLCOUNT]", report.Columns.Count.ToString());
+            baseHtml = baseHtml.Replace("[ROWCOUNT]", report.Rows.Count.ToString());
+
+            var baseColumnVal = $"\t<td class=\"tg-hmp3\">[COL]</td>{Environment.NewLine}";
+            var baseRowVal = $"\t<td class=\"tg-0lax\">[VAL]</td>{Environment.NewLine}";
+
+            if (usingTemplate)
+            {
+                baseRowVal = baseRowVal.Replace(" class=\"tg-0lax\"", string.Empty);
+                baseColumnVal = baseColumnVal.Replace(" class=\"tg-hmp3\"", string.Empty);
+            }
+
+            var fullColumnHtmlElement = string.Empty;
+            var fullRowHtmlElement = string.Empty;
+            report.Columns.ForEach(col =>
+            {
+                var replaced = baseColumnVal.Replace("[COL]", col.Name);
+                fullColumnHtmlElement += replaced;
+            });
+
+            report.Rows.ForEach(row =>
+            {
+                var baseRow = $"<tr>[ROWVAL]</tr>";
+                var fullRow = string.Empty;
+                var values = row.Values;
+
+                values.ForEach(val =>
+                {
+                    var replaced = baseRowVal.Replace("[VAL]", val.Value.ToString());
+                    fullRow += replaced;
+                });
+
+                baseRow = baseRow.Replace("[ROWVAL]", fullRow);
+                fullRowHtmlElement += baseRow;
+            });
+
+            baseHtml = baseHtml.Replace("[HEADERS]", fullColumnHtmlElement);
+            baseHtml = baseHtml.Replace("[RECORDS]", fullRowHtmlElement);
+
+            return baseHtml;
+        }
+    }
+}
