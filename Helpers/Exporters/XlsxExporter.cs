c7cad245e99bffb74eef02d7036e8e111c741274
Connor Jolley <connorjolley18@gmail.com>
2023-05-01 20:06:41 +0100

    Done the following:
    - [#25] Added ability to export to PDF
    - [#19] Added ability to specify Excel template for PDF Export
    - Added comments for missing XML comments on public methods

--- Helpers/Exporters/XlsxExporter.cs
+++ Helpers/Exporters/XlsxExporter.cs
@@ -1,22 +1,25 @@
-﻿using GeneralSQLReporter.Models;
-using Spire.Pdf.Exporting.XPS.Schema;
-using Spire.Xls;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading.Tasks;
-using Path = System.IO.Path;
-
-namespace GeneralSQLReporter.Helpers.Exporters
+﻿namespace GeneralSQLReporter.Helpers.Exporters
 {
+    using System;
+    using System.IO;
+    using Path = System.IO.Path;
+    using GeneralSQLReporter.Models;
+    using Spire.Xls;
+
+    /// <summary>
+    /// Interaction logic for the <see cref="XlsxExporter"/> Class.
+    /// </summary>
+    /// <remarks>
+    /// Used by the <see cref="SqlReportExporter"/> to generate Excel (xlsx) documents based on the <see cref="ReportResultSet"/>'s
+    /// </remarks>
     public static class XlsxExporter
     {
+        /// <summary>
+        /// <inheritdoc cref="SqlReportExporter.ExportExcel(ReportResultSet, string, bool, string, int, int, int, int, bool)"/>
+        /// </summary>
         internal static string ExportXlsx(ReportResultSet report, 
-            string templatePath,
-            bool overwrite,
+            string templatePath = null,
+            bool overwrite = true,
             string fileName = null,
             int sheetNumber = 0,
             int headerRow = 1,
@@ -26,8 +29,51 @@ namespace GeneralSQLReporter.Helpers.Exporters
         {
             SqlReportExporter.CheckExportFolder();
 
+            var workbook = XlsxExporter.GenerateWorkbook(report, 
+                templatePath, 
+                sheetNumber, 
+                headerRow, 
+                firstRow, 
+                firstCol, 
+                autofitCols);
+
+            var fullPath = string.IsNullOrWhiteSpace(fileName?.Trim()) ?
+                Path.Combine(SqlReportExporter.OutputDirectory(), $"{Guid.NewGuid()}.xlsx") :
+                Path.Combine(SqlReportExporter.OutputDirectory(), fileName);
+
+            if (File.Exists(fullPath) && overwrite)
+            {
+                File.Delete(fullPath);
+            }
+
+            workbook.SaveToFile(fullPath);
+
+            return fullPath;
+        }
+
+        /// <summary>
+        /// Generalised Exporting of the <see cref="ReportResultSet"/> into an Excel Format
+        /// </summary>
+        /// <param name="report">The <see cref="ReportResultSet"/> to export</param>
+        /// <param name="templatePath">The FilePath to a Template Excel file to use if applicable, if left empty will default to the Included Template</param>
+        /// <param name="sheetNumber">The Index for the Sheet to use, defaults to 0 (first sheet) if left empty</param>
+        /// <param name="headerRow">The Index for the Header Row, defaults to 1 if left empty</param>
+        /// <param name="firstRow">The Index of the First Row for the record, defaults to 2 if left empty</param>
+        /// <param name="firstCol">The Index of the FIrst Column for the headers/cell values, defaults to 1 if left empty</param>
+        /// <param name="autofitCols">Value to indicate whether to autofit columns or not for the report.</param>
+        /// <returns>The File Path for the Generated Excel Document</returns>
+        internal static Workbook GenerateWorkbook(ReportResultSet report, 
+            string templatePath = null,
+            int sheetNumber = 0,
+            int headerRow = 1,
+            int firstRow = 2,
+            int firstCol = 1,
+            bool autofitCols = true)
+        {
             var usingTemplate = !string.IsNullOrWhiteSpace(templatePath?.Trim());
+
             var workbook = new Workbook();
+
             if (usingTemplate)
             {
                 workbook.LoadFromFile(templatePath);
@@ -62,7 +108,7 @@ namespace GeneralSQLReporter.Helpers.Exporters
 
             var endCol = colIndex;
 
-            //// Copy row styling
+            //// Get range for the first row, to copy and styling set against it
             var rowRange = sheet.Range[firstRow, firstCol, firstRow, endCol];
 
             var rows = report.Rows;
@@ -92,19 +138,8 @@ namespace GeneralSQLReporter.Helpers.Exporters
                     firstCol++;
                 }
             }
-            
-            var fullPath = string.IsNullOrWhiteSpace(fileName?.Trim()) ?
-                Path.Combine(SqlReportExporter.OutputDirectory(), $"{Guid.NewGuid()}.xlsx") :
-                Path.Combine(SqlReportExporter.OutputDirectory(), fileName);
 
-            if (File.Exists(fullPath) && overwrite)
-            {
-                File.Delete(fullPath);
-            }
-
-            workbook.SaveToFile(fullPath);
-
-            return fullPath;
+            return workbook;
         }
     }
 }
